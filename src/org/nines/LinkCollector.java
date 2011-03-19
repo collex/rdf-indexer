c485d92d17f05e45976e587d00dafa08f6991c22
Lou Foster <lou@performantsoftware.com>
2011-03-19 14:05:14 +0000

    fixed to make unit tests work; added a no-param constructor that uses a printwriter-wrapped sys.out to write link data. Additiobally: if param constructor fails, it will default to sys.out based writer for data. at least the output wont be lost now.

--- src/org/nines/LinkCollector.java
+++ src/org/nines/LinkCollector.java
@@ -2,36 +2,66 @@ package org.nines;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
-public class LinkCollector {
-	
-	private FileWriter linkDataFile; 
+public class LinkCollector
+{
+    private Writer dataWriter;
 
-	public LinkCollector(String prefix) {
-		try {
-			linkDataFile = new FileWriter(prefix + "_link_data.txt", true);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void addLink( String documentURI, String filename, String url ) {
-		try {
-			linkDataFile.write(documentURI+"\t"+filename+"\t"+url+"\n");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void close( ) {
-		try {
-			linkDataFile.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
+    /**
+     * Testing LinkCollector that writes to System.out instead
+     * of a file
+     */
+    public LinkCollector()
+    {
+        dataWriter = new PrintWriter(System.out, true);
+    }
+    
+    /**
+     * Standard LinkCollector that writes data out to a link data
+     * file with the prefix specified. If the specified prefix does not
+     * exist, data will be streamed to System.out
+     * @param prefix
+     * @throws IOException Throws of the prefix does not exist
+     */
+    public LinkCollector(String prefix)
+    {
+        try
+        {
+            dataWriter = new FileWriter(prefix + "_link_data.txt", true);
+        }
+        catch (IOException e)
+        {
+            dataWriter = new PrintWriter(System.out, true);
+        }
+    }
+
+    public void addLink(String documentURI, String filename, String url)
+    {
+        try
+        {
+            dataWriter.write(documentURI + "\t" + filename + "\t" + url + "\n");
+            dataWriter.flush();
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void close()
+    {
+        try
+        {
+            dataWriter.close();
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 }
